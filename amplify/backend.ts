33d297ea5317db3410ed6da18cf47a98ff7920ab
Victor Lobato <pvlobato@gmail.com>
2024-10-29 20:28:06 +0100

    Add auth

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,15 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+const { cfnUserPool } = backend.auth.resources.cfnResources;
+cfnUserPool.policies = {
+  passwordPolicy: {
+    requireUppercase: false,
+    requireSymbols: false,
+  },
+}
--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 
 Amplify.configure(outputs);
 
@@ -30,6 +31,12 @@ export default function App() {
       content: window.prompt("Todo content"),
     });
   }
+    
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
+  const { signOut } = useAuthenticator();
 
   return (
     <main>
@@ -37,7 +44,10 @@ export default function App() {
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li
+            onClick={() => deleteTodo(todo.id)}
+            key={todo.id}>{todo.content}
+          </li>
         ))}
       </ul>
       <div>
@@ -47,6 +57,7 @@ export default function App() {
           Review next steps of this tutorial.
         </a>
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }

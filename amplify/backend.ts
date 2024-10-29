e771d4e62c7fb21490eb75f66fd88a1a85fe29e0
Victor Lobato <pvlobato@gmail.com>
2024-10-29 21:26:10 +0100

    Decrease password requirements

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,19 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
 
-defineBackend({
+const backend = defineBackend({
   auth,
-  data,
+  data
 });
+// extract L1 CfnUserPool resources
+const { cfnUserPool } = backend.auth.resources.cfnResources;
+// modify cfnUserPool policies directly
+cfnUserPool.policies = {
+  passwordPolicy: {
+    minimumLength: 8,
+    requireLowercase: false,
+    requireNumbers: false,
+    requireSymbols: false,
+    requireUppercase: false,
+  },
+};

ccc64428a94b276f4c7693f6b7dcdecd2662c6ee
Victor Lobato <pvlobato@gmail.com>
2024-10-29 20:40:36 +0100

    Undone stuff

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,15 +2,7 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
 
-const backend = defineBackend({
+defineBackend({
   auth,
   data,
 });
-
-const { cfnUserPool } = backend.auth.resources.cfnResources;
-cfnUserPool.policies = {
-  passwordPolicy: {
-    requireUppercase: false,
-    requireSymbols: false,
-  },
-}
